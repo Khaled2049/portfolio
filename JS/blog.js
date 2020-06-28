72b1d9ef3470c875b988502a219f4c7d29f2f4f1
Khaled Hossain <khaledhossain.not@gmail.com>
2020-06-28 15:25:06 -0500

    commit

---
+++ JS/blog.js
@@ -0,0 +1,6 @@
+const destiny = document.querySelector("#destiny");
+
+destiny.addEventListener("click", (element) => {
+    // document.querySelector(".element").classList.toggle("hidden");
+    element.target.classList.toggle("hidden"); 
+})
