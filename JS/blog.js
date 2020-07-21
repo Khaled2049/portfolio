d4c7a9223d80a72a6d5efd3e4015112f0e6a6261
Khaled Hossain <khaledhossain.not@gmail.com>
2020-07-20 23:24:32 -0500

    com

--- JS/blog.js
+++
@@ -1,6 +0,0 @@
-const destiny = document.querySelector("#destiny");
-
-destiny.addEventListener("click", (element) => {
-    // document.querySelector(".element").classList.toggle("hidden");
-    element.target.classList.toggle("hidden"); 
-})
---
+++ JS/writings.js
@@ -0,0 +1,108 @@
+let textShown = false;
+
+const destiny = `<h4>Destiny</h4>
+<p>
+  The fate of our lives is not predetermined but created with time. I am a firm believer that we make
+  our own destiny; it is not written down somewhere. There can be no doubt that life has a beginning and
+  an end. I want to focus more on the journey (living) which is more important and far more interesting
+  than the final destination (death). Along the way we learn things and make choices. We take actions
+  based on the decisions we make which in turn make us who we are. I don’t believe that we have fixed
+  destiny or a grand purpose in our lives. In the grand scheme of things, we are nothing but a spec but
+  that does not mean our existence is meaningless. The universe is infinite and there are thousands of
+  possibilities therefore, our destiny cannot be fixed but it connected with the fabric of the cosmos.
+</p>
+<p>
+  A lot of things depend on the cards we were dealt at the start of our lives. A good childhood, a
+  proper education and a strong will to not give up. The importance of strong morals and ethics cannot
+  be overlooked. The significance of our teachers teaching us the “how’s” and “why’s” of life. Were we
+  taught how to distinguish between good and evil? Were we taught to do the right thing without being
+  selfish? Were we taught to be kind and honest? Furthermore, the most important quality, in my opinion
+  is whether we have the ability to think. We need goals and objective and a mission in life. To be
+  successful and happy we must not give into temptations of simple pleasures and be patient and think
+  about the long term. All the while we must be aware, that nothing is fixed, and our mission will grow
+  and evolve with us. The things that we find important will also mutate. But certain goals will remain
+  same all throughout our lives.
+</p>
+<p>
+  The relationship between family and the bonds between friends are crucial. There will always be up’s
+  and downs but at the end of the day we need a safe place to come back to and unwind, the best place to
+  do that is with those whom you trust. There are times when solitude is important, but humans are
+  social creatures. Although, we need others we also need time to reflect by ourselves and to be alone
+  because the answers to some questions can only be discovered by self-reflection.
+</p>
+<p>
+  If I were to answer the question. What is your destiny? My answer would be simple. I have no clue. On
+  the other hand, if somebody were to ask me what do you want your destiny to be? I would say that I
+  want it to be someone that I can myself be proud of. I want it to be a story where I accomplish all my
+  goals, have a loving family, and be successful. I want it to be where I have helped others living
+  beings in some way and left something of value for future generations. I want it to be a life where I
+  experienced many things and have my enjoyed my time. I want it to be so that I have not hurt others
+  intentionally for my own selfish gains. I want my journey to be filled with hardship and struggle so
+  that I can grow and become better. Most importantly, I want to make my parents to be proud and happy.
+</p>
+<p>
+  What are my dreams? Most of dreams are meaningless and strange. When I close my eyes at night and go
+  to sleep, I never dream about success or happiness. Most of the time my dreams are weird and random.
+  For example, talking with animals or walking around and visiting nonexistent places. Therefore, I feel
+  the question can be rephrased into something else like ‘What do you want to do in this life?’
+</p>
+<p>
+  If I were to talk about my plan, about the things that I want to accomplish and do in this life than
+  it would become a long and complicated answer. But there are a couple of things which I will divulge.
+  I want to finish my undergraduate than get my masters because knowledge is important. Then I want to
+  work hard and save money so that I can retire early and stop working for money. I will still work but
+  my incentive will no longer be for money. I have no intention of becoming a rich man because money is
+  a social construct and we can never buy true happiness. I want to run my own business and be my own
+  boss, create something of value, and help those in need. I want to find someone whom I can respect and
+  love, spend the rest of my life with and create a family. Have friends and family that I enjoy
+  spending time with. Travelling the world isn’t something that is very important to me, but I wouldn’t
+  mind visiting a couple of places every now and then. I will be happy as long as I am close to nature.
+  Lastly, I want to teach others the things that I learn in this life so they themselves can live a life
+  worth living and make their own destiny.
+</p>
+<p>
+  Nothing will ever go perfectly according to plan, but we must be patient. Suffering is part of life
+  the sooner we realize it the better of we will be. Sometimes we expect a lot from other people in our
+  lives and then when they let us down, we become depressed and sad. We cannot control all the external
+  factors in our lives so I believe it is best to keep our expectations low, but we must never lose hope
+  because that would be giving up. We must try our best and even if we fail, we must keep trying to
+  achieve our goals and make our “dreams” a reality. To create the destiny that we want we must be ready
+  to make sacrifices. We must also realize that in the end the probability of our destiny being exactly
+  how we imagined it to be is very low. Coming back to the point I made in the first paragraph the end
+  is far less important than the journey. So even if our dreams and our imagined destiny isn’t exactly
+  how we wanted it to be, it will be okay. We must accept It because it is all we have, and it is the
+  best and only version we need.
+</p>`
+const knowledge = `Lorem ipsum dolor 
+sit amet consectetur adipisicing elit. 
+Rem, voluptatibus, esse velit suscipit explicabo aperiam rerum adipisci officia unde facere accusantium quia iste, at porro ut? Tempore rerum ullam dolorum unde exercitationem saepe beatae autem numquam asperiores, eligendi delectus id sed quae consequatur nihil dolores nemo facere est. Repellendus corrupti consequuntur voluptatem? Repudiandae esse obcaecati voluptates delectus iure tempora quod sed minima explicabo dignissimos distinctio neque eligendi voluptatem consectetur, corrupti veritatis facilis tempore temporibus earum aspernatur sint optio. Modi iusto tenetur nam quod architecto qui eaque harum quas, eligendi consequuntur fuga placeat exercitationem obcaecati deserunt ex blanditiis expedita consequatur alias. Ex, culpa assumenda numquam ipsa itaque minima delectus saepe id reiciendis sit tempore! Quos quia enim magni perspiciatis quam quae odit repellat distinctio velit expedita obcaecati, repellendus quo architecto, necessitatibus esse omnis molestias illo voluptatibus? Sunt, animi nobis quo earum exercitationem id deserunt nemo rerum doloribus architecto tempore, voluptatum blanditiis ut. Nulla porro distinctio odio libero similique accusantium obcaecati saepe blanditiis eius nostrum, voluptatum possimus reiciendis quam, hic soluta aliquid ad, ullam omnis. Eligendi repellendus architecto voluptatem quis placeat amet debitis aut totam inventore, nam fuga rem atque culpa officia deserunt a blanditiis voluptate nulla vero maiores dolorum ducimus! Reiciendis omnis magnam, at dicta, iste exercitationem aspernatur officiis sunt officia error mollitia corrupti quidem tenetur nobis. Harum quod architecto pariatur maxime quas. Debitis impedit alias aut repudiandae sit magnam qui quibusdam ea quis ad enim molestiae in error tempora sed, possimus quae pariatur architecto corporis doloremque laborum quia labore quam. Ipsam rerum, eum cum id impedit, minima repellat quibusdam, expedita molestiae dolore non! Aliquam, itaque repellat fuga eveniet facilis magnam. Quis sint animi perferendis repellendus? Error dolorem distinctio corporis repellat vel voluptas quibusdam, nam officiis nulla adipisci quisquam obcaecati voluptates nihil harum vitae atque suscipit eaque placeat quia! Perferendis iure dolorem, dicta maxime earum itaque quas, ad sapiente, accusamus corporis obcaecati sunt. Rem ipsam quidem fugiat expedita quae reprehenderit, sequi magnam nihil neque eum nisi officiis magni, ad eveniet sapiente voluptas beatae. Ex ipsam tempore ipsa quia culpa harum quam, est iure similique doloremque! Ex laboriosam, eveniet eum fuga numquam veritatis velit sequi quo neque dignissimos, nesciunt itaque, tempore illo dolor quam soluta repellat! Sint nulla mollitia quaerat dicta, repudiandae cum quam quidem qui eum eligendi iure, vel consectetur dolorum non possimus neque fuga quae. Itaque deleniti nemo natus, sed exercitationem corrupti at, qui doloribus blanditiis eum rerum ex quae consequuntur delectus, facere praesentium odio quisquam sit dolores ducimus quia repudiandae accusamus repellat! Delectus cum sit, accusantium consequatur in reprehenderit soluta veritatis porro accusamus eos assumenda deleniti. Nobis repudiandae aut at quibusdam omnis saepe inventore quos ipsam voluptatum veritatis? Maiores, iure unde quaerat explicabo nostrum sunt temporibus commodi voluptates sed quas quasi sequi qui eveniet veritatis omnis deleniti corporis quisquam, sit cum modi iusto laudantium harum ipsa. Vel reiciendis illum quod unde numquam sunt quis dicta provident amet at perferendis dolor excepturi eius architecto aliquam molestiae, fuga corporis id esse iste saepe voluptatum. Ad saepe voluptatibus libero aliquid praesentium vitae.`
+
+const story1 = document.querySelector('#destiny');
+const story2 = document.querySelector('#knowledge');
+
+const main = document.querySelector('#main');
+
+let showText = function(string){
+    let storyDiv = document.createElement('div');
+    main.appendChild(storyDiv);
+    storyDiv.innerHTML = string;
+
+}
+
+story1.addEventListener("click", () => {
+    if(textShown === true){
+        //remove text somehow
+        main.innerHTML = "";
+        textShown = false;
+    }
+    else{
+        showText(destiny);
+        textShown = true;
+    }
+    console.log("sup?");
+});
+
+
+
+
+
