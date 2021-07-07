992229ffdba64034456f0ee39883ae98e412c727
Khaled Hossain <khaledhossain.not@gmail.com>
2021-07-06 19:25:51 -0500

    Testing express

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+let posts = [];
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res){
+  res.render('index.html');
+});
+
+// app.listen(3000, function() {
+//     console.log("Server started on port 3000");
+// });
+  
