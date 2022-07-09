23ca1971030be37e70dd09345192767e8be92a86
Khaled Hossain <khaledhossain.not@gmail.com>
2022-07-09 14:08:11 -0500

    Added email function

---
+++ index.js
@@ -0,0 +1,23 @@
+const sendEmail = (values) => {
+  console.log('sending email');
+  const body = `Hey Khaled I'm FirstName: ${values.firstName} LastName: ${values.lastName} My phone number is: ${values.phone} and my email: ${values.email} and my message: ${values.msg}`;
+  window.open(
+    `mailto:khaledhossain.not@gmail.com?subject=Message From website&body=${body}`
+  );
+};
+
+const sendButton = document.querySelector('#sendButton');
+sendButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  let firstName = document.querySelector('#fname').value;
+  let lastName = document.querySelector('#lname').value;
+  let email = document.querySelector('#email').value;
+  let phone = document.querySelector('#phone').value;
+  let msg = document.querySelector('#message').value;
+
+  if (email !== '' && firstName !== '' && (lastName !== '') & (msg != '')) {
+    sendEmail({ firstName, lastName, email, phone, msg });
+  } else {
+    console.log('Enter Email, firstname, lastname & message');
+  }
+});
