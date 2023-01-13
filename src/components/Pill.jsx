52d2b32edffbc1337e64575eff058c8408fd4688
Khaled <khaledhossain.not@gmail.com>
2023-01-12 22:59:36 -0700

    Added skills

--- src/components/Pill.jsx
+++ src/components/Pill.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Pill = ({ text }) => {
   return (
-    <button className="rounded-md bg-red-500 px-4 py-2 text-white">
+    <div className="transform rounded-full bg-blue-400 w-1/8 m-3 p-3 hover:bg-blue-600 transition duration-500 hover:scale-125">
       {text}
-    </button>
+    </div>
   );
 };
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import Pill from "./Pill";
+
+const skills = ["React", "TS", "Python", "Golang", "AWS", "Git"];
+
 const Skills = () => {
   return (
     <div className="container mx-auto text-center p-5">
-      <h1>Skills</h1>
-      <div className="flex flex-wrap">
-        <Pill text="React" />
-        <Pill text="Typescript" />
+      <div className="grid md:grid-cols-2 gap-4 p-5 my-4">
+        <div className="border-solid border-2 flex flex-col justify-center items-center">
+          <span className="inline-block text-xl">Skills</span>
+        </div>
+
+        <div className="grid grid-cols-3 gap-4">
+          {skills.map((skill) => {
+            return <Pill key={skill} text={skill} />;
+          })}
+        </div>
       </div>
     </div>
   );
