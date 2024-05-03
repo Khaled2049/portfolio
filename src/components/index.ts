0c80d465ca99a70ab927cb4f36962101d9522d68
Khaled <khaledhossain.not@gmail.com>
2024-05-03 15:13:36 -0600

    Update

--- src/components/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as Footer } from "./Footer";
-export { default as Contact } from "./Contact";
-export { default as Projects } from "./Projects";
-export { default as Skills } from "./Skills";
-export { default as About } from "./About";
-export { default as Experience } from "./Experience";
-export { default as Navbar } from "./Navbar";
-export { default as ProjectCarousel } from "./ProjectCarousel";
-export { default as ProjectCard } from "./ProjectCard";
